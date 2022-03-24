fa5f05ae7ef1e9ac20afe88273e5d19361c2f6f2
Paolo Capriotti <paolo@capriotti.io>
2022-03-24 22:09:50 +0100

    Move tile setup to a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+mod tile;
+
 use glow::HasContext;
 use glutin::event::{Event, VirtualKeyCode};
 use glutin::event_loop::ControlFlow;
 use std::time::{Duration, Instant};
+use tile::Tile;
 
 fn main() {
     let eloop = glutin::event_loop::EventLoop::new();
@@ -17,117 +20,16 @@ fn main() {
     let gl =
         unsafe { glow::Context::from_loader_function(|s| win.get_proc_address(s) as *const _) };
 
-    let program = unsafe {
-        let vert = compile_shader_from_source(
-            &gl,
-            glow::VERTEX_SHADER,
-            r#"
-            #version 330 core
-            layout (location = 0) in vec3 p;
-
-            void main() {
-                gl_Position = vec4(p.x, p.y, p.z, 1.0);
-            }"#,
-        );
-
-        let frag = compile_shader_from_source(
-            &gl,
-            glow::FRAGMENT_SHADER,
-            r#"
-            #version 330 core
-
-            out vec4 col;
-
-            void main() {
-                col = vec4(1.0, 0.0, 0.0, 1.0);
-            }"#,
-        );
-
-        let program = gl.create_program().unwrap();
-        gl.attach_shader(program, vert);
-        gl.attach_shader(program, frag);
-        gl.link_program(program);
-        if !gl.get_program_link_status(program) {
-            panic!("link error: {}", gl.get_program_info_log(program));
-        }
-
-        gl.detach_shader(program, vert);
-        gl.delete_shader(vert);
-
-        gl.detach_shader(program, frag);
-        gl.delete_shader(frag);
-        program
-    };
-
-    let (vbo, vao) = unsafe {
-        let vertices: [f32; 20] = [
-            -0.3, -0.3, 0.0, 0.0, 1.0, // bottom left
-            0.3, -0.3, 0.0, 1.0, 1.0, // bottom right
-            -0.3, 0.3, 0.0, 0.0, 0.0, // top left
-            0.3, 0.3, 0.0, 1.0, 0.0, // top right
-        ];
-        let indices: [u32; 6] = [
-            0, 1, 2, // bottom left
-            2, 1, 3, // top right
-        ];
-        let vao = gl.create_vertex_array().unwrap();
-        let vbo = gl.create_buffer().unwrap();
-        let ebo = gl.create_buffer().unwrap();
-
-        gl.bind_vertex_array(Some(vao));
-
-        gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-        gl.buffer_data_u8_slice(
-            glow::ARRAY_BUFFER,
-            bytemuck::cast_slice(&vertices[..]),
-            glow::STATIC_DRAW,
-        );
-
-        // vertices
-        gl.vertex_attrib_pointer_f32(
-            0,
-            3,
-            glow::FLOAT,
-            false,
-            (std::mem::size_of::<f32>() * 5) as i32,
-            0,
-        );
-        gl.enable_vertex_attrib_array(0);
-
-        // uv
-        gl.vertex_attrib_pointer_f32(
-            1,
-            2,
-            glow::FLOAT,
-            false,
-            (std::mem::size_of::<f32>() * 5) as i32,
-            (std::mem::size_of::<f32>() * 3) as i32,
-        );
-        gl.enable_vertex_attrib_array(1);
-
-        gl.bind_buffer(glow::ARRAY_BUFFER, None);
-
-        gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
-        gl.buffer_data_u8_slice(
-            glow::ELEMENT_ARRAY_BUFFER,
-            bytemuck::cast_slice(&indices[..]),
-            glow::STATIC_DRAW,
-        );
-
-        gl.bind_vertex_array(None);
-        (vbo, vao)
-    };
+    let tile = Tile::new(&gl);
 
     eloop.run(move |e, _target, cf| {
         *cf = ControlFlow::WaitUntil(Instant::now() + Duration::from_millis(16));
         match e {
             Event::LoopDestroyed => unsafe {
-                gl.delete_buffer(vbo);
-                gl.delete_vertex_array(vao);
-                gl.delete_program(program);
+                tile.cleanup(&gl);
             },
             Event::RedrawRequested(_) => {
-                unsafe { draw_window(&gl, program, vao) };
+                unsafe { draw_window(&gl, &tile) };
                 win.swap_buffers().unwrap();
             }
             Event::WindowEvent { event: ref e, .. } => {
@@ -153,30 +55,9 @@ fn main() {
     });
 }
 
-unsafe fn draw_window(
-    gl: &glow::Context,
-    program: glow::NativeProgram,
-    vao: glow::NativeVertexArray,
-) {
+unsafe fn draw_window(gl: &glow::Context, tile: &Tile) {
     gl.clear_color(0.46, 0.7, 0.76, 1.0);
     gl.clear(glow::COLOR_BUFFER_BIT);
 
-    gl.use_program(Some(program));
-    gl.bind_vertex_array(Some(vao));
-    gl.draw_elements(glow::TRIANGLES, 6, glow::UNSIGNED_INT, 0);
-    gl.bind_vertex_array(None);
-}
-
-unsafe fn compile_shader_from_source(
-    gl: &glow::Context,
-    shader_type: u32,
-    src: &str,
-) -> glow::NativeShader {
-    let shader = gl.create_shader(shader_type).unwrap();
-    gl.shader_source(shader, src);
-    gl.compile_shader(shader);
-    if !gl.get_shader_compile_status(shader) {
-        panic!("{}", gl.get_shader_info_log(shader));
-    }
-    shader
+    tile.render(&gl);
 }
---
+++ src/tile.rs
@@ -0,0 +1,140 @@
+use glow::HasContext;
+
+pub struct Tile {
+    vbo: glow::NativeBuffer,
+    vao: glow::NativeVertexArray,
+    program: glow::NativeProgram,
+}
+
+impl Tile {
+    pub fn new(gl: &glow::Context) -> Self {
+        let (vbo, vao) = unsafe {
+            let vertices: [f32; 20] = [
+                -0.3, -0.3, 0.0, 0.0, 1.0, // bottom left
+                0.3, -0.3, 0.0, 1.0, 1.0, // bottom right
+                -0.3, 0.3, 0.0, 0.0, 0.0, // top left
+                0.3, 0.3, 0.0, 1.0, 0.0, // top right
+            ];
+            let indices: [u32; 6] = [
+                0, 1, 2, // bottom left
+                2, 1, 3, // top right
+            ];
+            let vao = gl.create_vertex_array().unwrap();
+            let vbo = gl.create_buffer().unwrap();
+            let ebo = gl.create_buffer().unwrap();
+
+            gl.bind_vertex_array(Some(vao));
+
+            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+            gl.buffer_data_u8_slice(
+                glow::ARRAY_BUFFER,
+                bytemuck::cast_slice(&vertices[..]),
+                glow::STATIC_DRAW,
+            );
+
+            // vertices
+            gl.vertex_attrib_pointer_f32(
+                0,
+                3,
+                glow::FLOAT,
+                false,
+                (std::mem::size_of::<f32>() * 5) as i32,
+                0,
+            );
+            gl.enable_vertex_attrib_array(0);
+
+            // uv
+            gl.vertex_attrib_pointer_f32(
+                1,
+                2,
+                glow::FLOAT,
+                false,
+                (std::mem::size_of::<f32>() * 5) as i32,
+                (std::mem::size_of::<f32>() * 3) as i32,
+            );
+            gl.enable_vertex_attrib_array(1);
+
+            gl.bind_buffer(glow::ARRAY_BUFFER, None);
+
+            gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
+            gl.buffer_data_u8_slice(
+                glow::ELEMENT_ARRAY_BUFFER,
+                bytemuck::cast_slice(&indices[..]),
+                glow::STATIC_DRAW,
+            );
+
+            gl.bind_vertex_array(None);
+            (vbo, vao)
+        };
+
+        let program = unsafe {
+            let vert = compile_shader_from_source(
+                &gl,
+                glow::VERTEX_SHADER,
+                r#"
+            #version 330 core
+            layout (location = 0) in vec3 p;
+
+            void main() {
+                gl_Position = vec4(p.x, p.y, p.z, 1.0);
+            }"#,
+            );
+
+            let frag = compile_shader_from_source(
+                &gl,
+                glow::FRAGMENT_SHADER,
+                r#"
+            #version 330 core
+
+            out vec4 col;
+
+            void main() {
+                col = vec4(1.0, 0.0, 0.0, 1.0);
+            }"#,
+            );
+
+            let program = gl.create_program().unwrap();
+            gl.attach_shader(program, vert);
+            gl.attach_shader(program, frag);
+            gl.link_program(program);
+            if !gl.get_program_link_status(program) {
+                panic!("link error: {}", gl.get_program_info_log(program));
+            }
+
+            gl.detach_shader(program, vert);
+            gl.delete_shader(vert);
+
+            gl.detach_shader(program, frag);
+            gl.delete_shader(frag);
+            program
+        };
+        Self { vbo, vao, program }
+    }
+
+    pub unsafe fn cleanup(&self, gl: &glow::Context) {
+        gl.delete_buffer(self.vbo);
+        gl.delete_vertex_array(self.vao);
+        gl.delete_program(self.program);
+    }
+
+    pub unsafe fn render(&self, gl: &glow::Context) {
+        gl.use_program(Some(self.program));
+        gl.bind_vertex_array(Some(self.vao));
+        gl.draw_elements(glow::TRIANGLES, 6, glow::UNSIGNED_INT, 0);
+        gl.bind_vertex_array(None);
+    }
+}
+
+unsafe fn compile_shader_from_source(
+    gl: &glow::Context,
+    shader_type: u32,
+    src: &str,
+) -> glow::NativeShader {
+    let shader = gl.create_shader(shader_type).unwrap();
+    gl.shader_source(shader, src);
+    gl.compile_shader(shader);
+    if !gl.get_shader_compile_status(shader) {
+        panic!("{}", gl.get_shader_info_log(shader));
+    }
+    shader
+}
