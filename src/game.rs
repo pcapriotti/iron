4795808a0f276ec77dbe22887badcc76fce0cf53
Paolo Capriotti <paolo@capriotti.io>
2022-04-15 20:42:18 +0200

    Implement game step

--- src/game.rs
+++ src/game.rs
@@ -4,10 +4,18 @@ pub type Value = u8;
 
 /// State of the game.
 #[allow(dead_code)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct Game {
     width: usize,
     height: usize,
     tiles: Vec<Option<Value>>,
+}
+
+pub enum Direction {
+    E,
+    N,
+    W,
+    S,
 }
 
 impl Game {
@@ -54,5 +62,210 @@ impl Game {
         self.tiles.iter().enumerate().filter_map(|(i, v)| {
             v.map(|v| ((i % self.width(), i / self.width()), v))
         })
+    }
+
+    pub fn step(&mut self, dir: Direction) {
+        let (width, height) = match dir {
+            Direction::S | Direction::N => (self.width(), self.height()),
+            Direction::E | Direction::W => (self.height(), self.width()),
+        };
+
+        let get: Box<dyn Fn(usize, usize) -> usize> = match dir {
+            Direction::E => Box::new(|x, y| height - y - 1 + x * width),
+            Direction::N => Box::new(|x, y| x + (height - y - 1) * width),
+            Direction::W => Box::new(|x, y| y + x * width),
+            Direction::S => Box::new(|x, y| x + y * width),
+        };
+
+        for x in 0..width {
+            let mut y0 = 0;
+
+            for y1 in 0..height {
+                if let Some(v) = self.tiles[get(x, y1)] {
+                    if y0 == y1 {
+                        continue;
+                    }
+                    self.tiles[get(x, y1)] = None;
+                    match self.tiles[get(x, y0)] {
+                        None => {
+                            self.tiles[get(x, y0)] = Some(v);
+                        }
+                        Some(w) if w == v => {
+                            self.tiles[get(x, y0)] = Some(v + 1);
+                            y0 += 1;
+                        }
+                        Some(_) => {
+                            y0 += 1;
+                            self.tiles[get(x, y0)] = Some(v);
+                        }
+                    };
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_step_south_trivial() {
+        let mut game = Game::new(4, 4);
+        game.tiles[0] = Some(0);
+        let game2 = game.clone();
+        game.step(Direction::S);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_east_trivial() {
+        let mut game = Game::new(4, 4);
+        game.tiles[3] = Some(2);
+        let game2 = game.clone();
+        game.step(Direction::E);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_north_trivial() {
+        let mut game = Game::new(4, 4);
+        game.tiles[13] = Some(2);
+        let game2 = game.clone();
+        game.step(Direction::N);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_west_trivial() {
+        let mut game = Game::new(4, 4);
+        game.tiles[0] = Some(2);
+        let game2 = game.clone();
+        game.step(Direction::W);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_south_simple() {
+        let mut game = Game::new(4, 4);
+        game.tiles[4] = Some(0);
+        game.tiles[9] = Some(1);
+        game.step(Direction::S);
+
+        let mut game2 = Game::new(4, 4);
+        game2.tiles[0] = Some(0);
+        game2.tiles[1] = Some(1);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_east_simple() {
+        let mut game = Game::new(4, 4);
+        game.tiles[4] = Some(0);
+        game.tiles[9] = Some(1);
+        game.step(Direction::E);
+
+        let mut game2 = Game::new(4, 4);
+        game2.tiles[7] = Some(0);
+        game2.tiles[11] = Some(1);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_north_simple() {
+        let mut game = Game::new(4, 4);
+        game.tiles[4] = Some(0);
+        game.tiles[9] = Some(1);
+        game.step(Direction::N);
+
+        let mut game2 = Game::new(4, 4);
+        game2.tiles[12] = Some(0);
+        game2.tiles[13] = Some(1);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_west_simple() {
+        let mut game = Game::new(4, 4);
+        game.tiles[4] = Some(0);
+        game.tiles[9] = Some(1);
+        game.step(Direction::W);
+
+        let mut game2 = Game::new(4, 4);
+        game2.tiles[4] = Some(0);
+        game2.tiles[8] = Some(1);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_merge() {
+        let mut game = Game::new(4, 4);
+        game.tiles[4] = Some(0);
+        game.tiles[8] = Some(0);
+        game.step(Direction::S);
+
+        let mut game2 = Game::new(4, 4);
+        game2.tiles[0] = Some(1);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_double_merge() {
+        let mut game = Game::new(4, 4);
+        game.tiles[0] = Some(1);
+        game.tiles[4] = Some(1);
+        game.tiles[8] = Some(3);
+        game.tiles[12] = Some(3);
+        game.step(Direction::S);
+
+        let mut game2 = Game::new(4, 4);
+        game2.tiles[0] = Some(2);
+        game2.tiles[4] = Some(4);
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_stuck() {
+        let mut game = Game::new(4, 4);
+        game.tiles[1] = Some(1);
+        game.tiles[5] = Some(2);
+        game.tiles[9] = Some(3);
+        game.tiles[13] = Some(4);
+        let game2 = game.clone();
+        game.step(Direction::S);
+
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_merge_in_place() {
+        let mut game = Game::new(4, 4);
+        game.tiles[1] = Some(1);
+        game.tiles[5] = Some(1);
+        game.tiles[9] = Some(2);
+        game.tiles[13] = Some(2);
+        game.step(Direction::S);
+
+        let mut game2 = Game::new(4, 4);
+        game2.tiles[1] = Some(2);
+        game2.tiles[5] = Some(3);
+
+        assert_eq!(game, game2);
+    }
+
+    #[test]
+    fn test_step_merge_in_place_equal() {
+        let mut game = Game::new(4, 4);
+        game.tiles[1] = Some(1);
+        game.tiles[5] = Some(1);
+        game.tiles[9] = Some(1);
+        game.tiles[13] = Some(1);
+        game.step(Direction::S);
+
+        let mut game2 = Game::new(4, 4);
+        game2.tiles[1] = Some(2);
+        game2.tiles[5] = Some(2);
+
+        assert_eq!(game, game2);
     }
 }
